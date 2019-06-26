3f18f482526c1f68ad718d5b3ce276d262876b55
bobbywlindsey <me@bobbywlindsey.com>
2019-06-25 23:41:08 -0500

    Add support for sklearn via lambda layers

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,22 +1,39 @@
 #!/bin/bash
 
-# remove the file if it exists
-echo "Removing zip file if exists..."
-rm -f ./src/function.zip
+# make a scikit-learn zip to be added as a layer
+cd layers/sklearn
+docker run --rm -v $PWD:/var/task lambci/lambda:build-python3.7 \
+pip install scikit-learn --no-deps -t python/lib/python3.7/site-packages/
+zip -r9q sklearn-layer.zip .
+# upload it to s3
+echo "Uploading sklearn layer to s3..."
+aws s3 cp sklearn-layer.zip s3://bobbywlindseytest
+# cleanup
+rm -f sklearn-layer.zip
+rm -rf python/
+cd ../..
 
+# make a joblib zip to be added as a layer
+cd layers/joblib
+docker run --rm -v $PWD:/var/task lambci/lambda:build-python3.7 \
+pip install joblib --no-deps -t python/lib/python3.7/site-packages/
+zip -r9q joblib-layer.zip .
+# upload it to s3
+echo "Uploading joblib layer to s3..."
+aws s3 cp joblib-layer.zip s3://bobbywlindseytest
+# cleanup
+rm -f joblib-layer.zip
+rm -rf python/
+cd ../..
 
-# then zip the code
+# zip the python lambda code
 echo "Zipping the code..."
 cd src/
 zip function.zip function.py
-
-# now upload zip of lambda function using cli
-# since CloudFormation doesn't have access
-# to file system
+# upload it to s3
 echo "Uploading code to s3..."
-aws s3 cp *.zip s3://bobbywlindseytest
-
-# remove the zip file
+aws s3 cp function.zip s3://bobbywlindseytest
+# cleanup
 echo "Removing zip file..."
 rm -f function.zip
 
