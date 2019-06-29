eaec65a0e8b2403a3ba7ce06dc83dffded74b290
bobbywlindsey <me@bobbywlindsey.com>
2019-06-29 15:44:58 -0500

    Fix lots of bugs

--- src/function.py
+++ src/function.py
@@ -3,21 +3,35 @@ import numpy as np
 from joblib import load
 import boto3
 from io import BytesIO
+import json
+
+def response(message, status_code):
+    return {
+        'statusCode': str(status_code),
+        'body': json.dumps(message),
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        },
+    }
 
 def lambda_handler(event, context):
     # import model from s3
     s3 = boto3.resource('s3')
     with BytesIO() as model:
         s3.Bucket("bobbywlindseytest").download_fileobj("regression_model.joblib", model)
-        model.seek(0)    # move back to the beginning after writing
+        model.seek(0) # move back to the beginning after writing
         regression_model = load(model)
 
     # get user input
-    usr_input = event['input']
+    request_body = event['body']
+    # if request came from api gateway
+    if isinstance(event['body'], (str)):
+        request_body = json.loads(request_body)
+    usr_input = request_body['input']
 
     # make predictions using the testing set
     diabetes_y_pred = regression_model.predict([[usr_input]])
 
     # return predictions
-    return(str(diabetes_y_pred[0]))
-
+    return response({'prediction': diabetes_y_pred[0]}, 200)
--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-# make a scikit-learn zip to be added as a layer
-cd layers/sklearn
-docker run --rm -v $PWD:/var/task lambci/lambda:build-python3.7 \
-pip install scikit-learn --no-deps -t python/lib/python3.7/site-packages/
-zip -r9q sklearn-layer.zip .
-# upload it to s3
-echo "Uploading sklearn layer to s3..."
-aws s3 cp sklearn-layer.zip s3://bobbywlindseytest
-# cleanup
-rm -f sklearn-layer.zip
-rm -rf python/
-cd ../..
-
 # make a joblib zip to be added as a layer
 cd layers/joblib
 docker run --rm -v $PWD:/var/task lambci/lambda:build-python3.7 \
@@ -23,6 +10,19 @@ echo "Uploading joblib layer to s3..."
 aws s3 cp joblib-layer.zip s3://bobbywlindseytest
 # cleanup
 rm -f joblib-layer.zip
+rm -rf python/
+cd ../..
+
+# make a scikit-learn zip to be added as a layer
+cd layers/sklearn
+docker run --rm -v $PWD:/var/task lambci/lambda:build-python3.7 \
+pip install scikit-learn --no-deps -t python/lib/python3.7/site-packages/
+zip -r9q sklearn-layer.zip .
+# upload it to s3
+echo "Uploading sklearn layer to s3..."
+aws s3 cp sklearn-layer.zip s3://bobbywlindseytest
+# cleanup
+rm -f sklearn-layer.zip
 rm -rf python/
 cd ../..
 
